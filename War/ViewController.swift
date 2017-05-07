90023dbb42f9b1e27650d9309c83f5f080bc403b
Gopikrishna Saripuri <gopis@onecloudinc.com>
2017-05-07 13:02:16 -0700

    Initial Commit

---
+++ War/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by gopikrishna saripuri on 5/7/17.
+//  Copyright © 2017 gopikrishna saripuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
