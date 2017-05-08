9aa2074d79be83785a41caae01a1050c83c05545
Gopikrishna Saripuri <gopis@onecloudinc.com>
2017-05-08 13:02:16 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Previously worked on iOS app development using Objective-C, but that a while ago.
+I think its time to get back to developing iOS applications using Swift.
+
+Note:
+I've tried this simple project with the help of codewithchris.com tutorial. 
+You can check his youtube channel, it was very well explained.
--- War/ViewController.swift
+++ War/ViewController.swift
@@ -10,9 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    var rightScore:Int = 0
+    var leftScore:Int = 0
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    let cardNames = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        print("Deal tapped")
+        let leftNumber:Int = Int(arc4random_uniform(13))
+        let rightNumber:Int = Int(arc4random_uniform(13))
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
+        if leftNumber > rightNumber {
+            // Leftcard wins
+            leftScore += 1
+            
+            leftScoreLabel.text = String(leftScore)
+        } else if leftNumber == rightNumber {
+            
+        } else {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        
+        if leftScore == 10 || rightScore == 10 {
+            var message:String
+            if leftScore > rightScore {
+                message = "Player Wins by " + String(leftScore - rightScore) + " points."
+            } else {
+                message = "CPU Wins by " + String(rightScore - leftScore) + " points."
+            }
+            
+            let alert = UIAlertController(title: "GameOver", message: message, preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Done", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            rightScoreLabel.text = String(0)
+            leftScoreLabel.text = String(0)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
